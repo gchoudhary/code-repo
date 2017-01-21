cae64596786a0e8b55e3fe89216a6ef638bdae6d
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2017-01-21 13:55:58 +0530

    Initial Commit

---
+++ datastructure/src/graph/Graph.java
@@ -0,0 +1,50 @@
+package graph;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class Graph<T> {
+	Map<T, List<T>> graph;
+
+	Graph() {
+		graph = new HashMap<>();
+	}
+
+	public void addEdge(T source, T dest) {
+		if (!graph.containsKey(source)) {
+			List<T> list = new LinkedList<T>();
+			graph.put(source, list);
+		}
+		if (!graph.containsKey(dest)) {
+			List<T> list = new LinkedList<T>();
+			graph.put(dest, list);
+		}
+
+		List<T> l = graph.get(source);
+		l.add(dest);
+		l = graph.get(dest);
+		l.add(source);
+	}
+
+	public void traverse() {
+		for (T i : graph.keySet()) {
+			System.out.print(i + " -> ");
+			for (T l : graph.get(i)) {
+				System.out.print(l + " -> ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		Graph<Integer> g = new Graph();
+		g.addEdge(1, 5);
+		g.addEdge(2, 5);
+		g.addEdge(3, 4);
+		g.addEdge(3, 5);
+		g.addEdge(3, 1);
+		g.traverse();
+	}
+}
